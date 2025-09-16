761dd3a7f1b1f900b6d0b699686f7669cb9254df
drema3-4 <drema3-4>
2025-09-16 20:07:41 +0300

    Добавил комментарии в processing_text

--- src/processing_text.py
+++ src/processing_text.py
@@ -4,35 +4,49 @@ import string
 import spacy
 import numpy as np
 
-
 class Prepare_Text:
+    '''Класс для подготовки текста перед загрузкой его в индекс.
+
+    Есть два вида подготовки:
+        1. Лёгкая версия: очитска от лишних пробелов и переносов строк;
+        2. Полновесная версия: легкая версия + токенизация, очистка от стоп-слов, очистка от неалфавитных токенов.'''
     def __init__(self):
+        '''Функция инициализации.
+
+        Загружает языковые пакеты библиотеки SpaCy.'''
         self.nlp_en = spacy.load("en_core_web_sm")
         self.nlp_ru = spacy.load("ru_core_news_sm")
 
-    def prepare_text(self, text: str) -> str:
-        return self.__processing_cell__(text)
-
     def light_prepare_text(self, text: str) -> str:
+        '''Функция для облегчённой подготовки (удаление лишних пробелов
+        и переносов строк) строки.
+
+        Args:
+            text: строка, которую нужно подготовить.
+
+        Returns:
+            str: подготовленная строка (без лишних пробелов и переносов строк).'''
         if type(text) == str and len(text) > 0:
             return self.__remove_extra_spaces_and_line_breaks__(text)
         else:
             return ""
 
-    def prepare_docs(
-        self, path: str, columns: list[str], processing_columns: list[str],
-        num_of_docs: int
-    ) -> pd.DataFrame:
-        docs = pd.read_excel(path)
-        docs = docs.loc[:num_of_docs, columns]
-        docs = docs.fillna("")
-        docs = docs.astype(str)
-
-        return self.__processing__(docs, processing_columns)
-
     def light_prepare_docs(
         self, path: str, columns: list[str], num_of_docs: int
     ) -> pd.DataFrame:
+        '''Функция для облегчённого варианта подготовки (удаление лишних
+        пробелов и переносов строк) pandas DataFrame.
+
+        Args:
+            path: путь к файлу (формата excel) для загрузки его в
+            pandas DataFrame.
+            columns: столбцы, которые нужно будет загрузить в pandas DataFrame.
+            num_of_docs: количество строк, которые нужно будет загрузить в
+            pandas DataFrame.
+
+        Returns:
+            pd.DataFrame: возвращает подготовленный DataFrame, не содержащий
+            пустых строк.'''
         docs = pd.read_excel(path)
         docs = docs.loc[:num_of_docs, columns]
         docs = docs.fillna("")
@@ -50,16 +64,55 @@ class Prepare_Text:
                 else:
                     docs.loc[row, column] = ""
 
+        docs = docs.replace(['', ' '], np.nan).dropna(how='all').reset_index(drop=True)
+
         return docs
 
-    def __first_is_en__(self, cell: str) -> bool:
-        '''
-        Определяет начинается строка с символа русского алфавита или
-        английского алфавита.\n
-        cell: строка.\n
-        Возвращает true, если строка начинается с символа английского алфавита.
-        '''
+    def prepare_text(self, text: str) -> str:
+        '''Функция для полноценной подготовки одной строки.
 
+        Args:
+            text: строка, которую нужно будет подготовить.
+
+        Returns:
+            str: возвращает подготовленную функцией __processing__ строку.'''
+        return self.__processing_cell__(text)
+
+    def prepare_docs(
+        self, path: str, columns: list[str], processing_columns: list[str],
+        num_of_docs: int
+    ) -> pd.DataFrame:
+        '''Функция вызова обработки pandas DataFrame.
+
+            Args:
+                path: путь к файлу с данными (в формате excel), которые
+                будут загружены в pandas DataFrame.
+                columns: список столбцов, которые нужно загрузить из файла.
+                processing_columns: список столбцов, которые нужно будет
+                обработать в pandas DataFrame.
+                num_of_docs: сколько строк загрузить из файла.
+
+            Returns:
+                pd.DataFrame: возвращает pandas DataFrame, подготовленный функцией
+                __processing__.'''
+        docs = pd.read_excel(path)
+        docs = docs.loc[:num_of_docs, columns]
+        docs = docs.fillna("")
+        docs = docs.astype(str)
+
+        return self.__processing__(docs, processing_columns)
+
+
+
+    def __first_is_en__(self, cell: str) -> bool:
+        '''Функция определяет алфавит первого символа строки.
+
+        Args:
+            cell: строка, для которой будут происходить вычисления.
+
+        Returns:
+            bool: возвращает True, если первый символ принадлежит английскому алфавиту,
+            False в противном случае.'''
         index_first_en = re.search(r"[a-zA-Z]", cell)
         index_first_ru = re.search(r"[а-яА-Я]", cell)
 
@@ -69,16 +122,16 @@ class Prepare_Text:
         ) else False
 
     def __split_into_en_and_ru__(self, cell: str) -> list[(bool, str)]:
-        '''
-        Разделяет строку на части, в которых содержатся символы принадлежащие
-        только русскому или английскому алфавиту (то есть в строке с русскими
-        символами не будет символов английского языка и наоборот, остальные символы
-        не удаляются).\т
-        cell: строка.\n
-        Возврщает массив кортежей
-        (True(если начинается с символа английского алфавита), подстрока).
-        '''
+        '''Разделяет строку на части, состоящие только из одного алфавита (русского
+        или английскому.
 
+        Args:
+            cell: строка, которая будет разбиваться на части.
+
+        Returns:
+            list[(bool, str)]: (True (если алфавит английский), подстрока строки).
+            В виде результата возвращается массив кортежей, в которых первое значение
+            указывает тип алфавита строки, а второе содержит подстроку исходной строки.'''
         parts = []
         is_en = self.__first_is_en__(cell)
         part = ""
@@ -96,12 +149,14 @@ class Prepare_Text:
         return parts
 
     def __remove_extra_spaces_and_line_breaks__(self, text: str) -> str:
-        '''
-        Удаляет из строки лишние пробелы и переносы строки.\n
-        text: строка.\n
-        Возврщает строку, с удалёнными лишними пробелами и переносами строк.
-        '''
+        '''Удаляет из строки лишние пробелы и переносы строк, приводя её к
+        читабельному виду.
 
+        Args:
+            text: строка, которая будет очищаться.
+
+        Returns:
+            str: возвращает очищенную от лишних пробелов и переносов строку.'''
         processed = ""
 
         if type(text) != str or len(text) == 0:
@@ -121,87 +176,62 @@ class Prepare_Text:
 
         return processed.strip()
 
-    def __time_processing__(self, text: str) -> str:
-        '''
-        Обрабатывает строку, содержащую дату.\n
-        text: строка, содержащая время в формате dd:dd.\n
-        Возвращает вместо времени строку time.
-        '''
-
-        if re.match(r"\d{2}:\d{2}", text):
-            return "time"
-        else:
-            return text.replace(":", "")
-
-    def __processing_token__(self, token_lemma: str) -> str:
-        '''
-        Обрабатывает 1 терм.\n
-        token_lemma: строка.\n
-        Возвращает обработанный терм.
-        '''
-
-        return self.__time_processing__(
-            self.__remove_extra_spaces_and_line_breaks__(token_lemma)
-        )
-
     def __processing_cell__(self, cell: str) -> str:
-        '''
-        Полностью обрабатывает 1 ячейку pandas DataFrame. То есть проводит
-        токенизацию, лемматизацию, удаление стоп слов и перевод в нижний регистр,
-        потом происходит склейка и возвращается обработанная ячейка.\n
-        cell: строка - ячейка pandas DataFrame.\n
-        Возвращает обработанную строку.
-        '''
+        '''Обработка одной ячейки pandas DataFrame.
 
-        parts = self.__split_into_en_and_ru__(cell)
+        Args:
+            cell: ячейка pandas DataFrame (предполагаемо строкового типа.
+
+        Returns:
+            str: возвращает строку, очищенную от лишних пробелов и переносов
+            строк + без стоп-слов, кроме того текст, кроме наименований,
+            переводится в начальную форму и нижний регистр.'''
+        parts = self.__split_into_en_and_ru__(
+            self.__remove_extra_spaces_and_line_breaks__(cell)
+        )
 
         tokens = []
 
         for part in parts:
             if part[0]:
                 tokens += [
-                    self.__processing_token__(token.lemma_)
+                    token.lemma_
                     for token in self.nlp_en(part[1])
                     if not (token.is_stop) and not (token.is_punct) and
-                    len(self.__processing_token__(token.lemma_)) > 1
+                    len(token.lemma_) > 1
                 ]
             else:
                 tokens += [
-                    self.__processing_token__(token.lemma_)
+                    token.lemma_
                     for token in self.nlp_ru(part[1])
                     if not (token.is_stop) and not (token.is_punct) and
-                    len(self.__processing_token__(token.lemma_)) > 1
+                    len(token.lemma_) > 1
                 ]
 
-        return " ".join(tokens)
+        return " ".join(tokens) if tokens else ""
 
     def __processing__(
         self, data: pd.DataFrame, processing_columns: list[str]
     ) -> pd.DataFrame:
-        '''
-        Функция, вызывающая вышеописанные функции для обработки pandas DataFrame.\n
-        data: pandas DataFrame;\n
-        processing_columns: list[str] - список столбцов, которые нужно обрабатывать.\n
-        Возвращает обработанный pandas DataFrame.
-        '''
+        '''Обработка целого pandas DataFrame.
+
+        Args:
+            data: pandas DataFrame, который нужно обработать.
+            processing_columns: список наименований столбцов pandas DataFrame,
+            которые нужно обработать.
+
+        Returns:
+            pd.DataFrame: Возвращает pandas DataFrame, ячейки которого (только
+            столбцов processing_columns) подготовлены функцией __processing_sell__.
+            Строки, которые окажутся пустыми после очистки будут удалены.'''
 
         p_data = data.copy(deep=True)
         p_data.fillna("", inplace=True)
 
-        for column in processing_columns:
-            for row in range(p_data.shape[0]):
-                cell = p_data.loc[row, column]
+        for row in range(p_data.shape[0]):
+            for column in processing_columns:
+                p_data.loc[row, column] = self.__processing_cell__(p_data.loc[row, column])
 
-                if type(cell) == str and len(cell) > 0:
-                    p_data.loc[row, column] = self.__processing_cell__(
-                        p_data.loc[row, column]
-                    )
-                else:
-                    p_data.loc[row, column] = ""
+        p_data = p_data.replace(['', ' '], np.nan).dropna(how='all').reset_index(drop=True)
 
-        # return p_data[p_data.apply(
-        #     lambda row: sum([len(cell)
-        #                      for cell in row[processing_columns]]) > 0,
-        #     axis=1
-        # )].reset_index(drop=True)
         return p_data
